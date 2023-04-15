00e97ff84d74209b379bc39f1631c326b631ccdd
Jim Conner <jimconner@users.noreply.github.com>
2023-04-15 23:57:30 +0100

    Add support for creating apps directly from numbered ROM banks.

--- make_app.py
+++ make_app.py
@@ -1,32 +1,55 @@
 #!/usr/bin/env python
 
 # A tool for helping to build Z88 Compressed app files.
-# This code reads in a bunch of lz49 compressed app files, creates an app header and writes them all together into a single app.
+# This code reads in a bunch of either .63,.62 ROM files or lz49 compressed app files, 
+# creates an app header and writes them all together into a single app.
 
 import glob
 import os
+import subprocess
 import struct
 import sys
 from colorama import just_fix_windows_console
 from termcolor import colored
 
 if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} <rom_name>")
+    print(f"Error: No ROM path supplied.")
+    print(f"{sys.argv[0]} - A tool for building compressed RAM apps from ROM image or RAM application banks.")
+    print(f"Finds all associated bank files, compresses with lz49, creates an app header and joins them all toegther.")
+    print(f"Resulting .app file is saved in the same place as the path to the ROM/App you provide.")
+    print(f"Usage: {sys.argv[0]} <path_to_63_rom> e.g. {sys.argv[0]} mmz88/mmz88.63")
+    print(f"Usage: {sys.argv[0]} <path_to_ap0_file> e.g. {sys.argv[0]} zchess/zchess.ap0")
+    sys.exit(1)
 
 infile=sys.argv[1]
 dirpath,filename=os.path.split(infile)
 prefix=filename.split('.')[0]
-romfiles=glob.glob(f"{dirpath}/{prefix}.lz[0123456789]")
+romfiles=glob.glob(f"{dirpath}/{prefix}.[0123456][0123456789]")
+appfiles=glob.glob(f"{dirpath}/{prefix}.ap[0123456789]")
+suffix_offset=0
+reverse_sort=True
+if len(romfiles) == 0:
+    romfiles = appfiles
+    suffix_offset = 2
+    reverse_sort=False
+for file in romfiles:
+    name, suffix=file.split('.')
+    filenum=suffix[suffix_offset:]
+    status,output = subprocess.getstatusoutput(f'lz49 -v -i {file} -o {name}.lz{filenum}')
+print(output)
+
+lzfiles=glob.glob(f"{dirpath}/{prefix}.lz*")
 
 app_header=b'\xa5\x5a'
 
 # Read all of the pages into a dict for in-memory manipulation.
 # Page number is the key, binary rom data is the value
 rom_image={}
-for file in sorted(romfiles):
+for file in sorted(lzfiles, reverse=reverse_sort):
     print(f"Reading {file}")
-    suffix=file.split('.')
+    name, suffix=file.split('.')
     rom_image[suffix]=open(file, mode='rb').read()
+    os.remove(file)
 
 if len(rom_image) < 1:
     print("No ROMS loaded. Exiting.")
